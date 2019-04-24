329e6831ce8adf7ab5fb2a1e01704ee17042ecce
abecel22 <abe2211@gmail.com>
2019-04-24 06:05:50 -0500

    Created file upload form and state for file.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
+import FileUpload from "./components/fileUpload";
 import "./App.css";
 
-const App = () => <div className="App">app</div>;
+const App = () => (
+  <div className="container mt-4">
+    <h4 className="display-4 text-center mb-4">
+      <i className="fab fa-react" />
+      REX File Upload
+    </h4>
+
+    <FileUpload />
+  </div>
+);
 
 export default App;
